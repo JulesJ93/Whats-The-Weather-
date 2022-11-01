3132e9a8e9ddd38caaeda1fc8505eb43772afcec
Jules Joseph <julesj93@gmail.com>
2022-11-01 17:07:20 -0400

    added variables to script to use openweather api

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+//this are variables that I will call on as functions below.
+var cityNameEl = document.querySelector("#name");
+var curDateEl = document.querySelector("#date");
+var curIconEl = document.querySelector("#icon");
+var curTempEl = document.querySelector("#temp");
+var curHumidityEl = document.querySelector("#humidity");
+var curWindEl = document.querySelector("#wind");
+var curUvEl = document.querySelector("#uv");
+var searchInputEl = document.querySelector("#search-city");
+var formEl = document.querySelector("#search-form");
+var historyEl = document.querySelector("#history");
+var clearBtnEl = document.querySelector("#clear-history");
+var forecastEl = document.querySelector("#forecast-body");
+var resultsContEl = document.querySelector("#results-container");
+var forecastContEl = document.querySelector("#forecast-container");
+var curStatsEl = document.querySelector("#current-stats");
+
 //api key used for this project from open weather\
 var apikey ="400cea51515fe3ec6686bdc773f2593a"
+
+//array for storing the previously searched cities
+var searchHistory = [];
+
+//this variable sets up the forecast for each day
+var forecast={};
 
 //this variable is to set up current weather
 var currentweather = {
@@ -12,9 +35,3 @@ var currentweather = {
     uvAlert: "",
     icon: ""
 }
-
-//array for storing the previously searched cities
-var searchHistory = [];
-
-//this variable sets up the forecast for each day
-var forecast={};
