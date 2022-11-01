f06dce5a2c0f9ee79ebbeeddec4c74132abef842
Jules Joseph <julesj93@gmail.com>
2022-11-01 16:58:29 -0400

    added current weather variable

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //api key used for this project from open weather\
 var apikey ="400cea51515fe3ec6686bdc773f2593a"
+
+//this variable is to set up current weather
+var currentweather = {
+    name: "",
+    date: "",
+    temp: "",
+    humidity: "",
+    wind: "",
+    uv: "",
+    uvAlert: "",
+    icon: ""
+}
